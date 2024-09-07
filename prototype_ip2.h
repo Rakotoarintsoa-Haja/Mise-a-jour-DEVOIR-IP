31fd8c66ff541e01f020d97958c0068a19328df0
Rakotoarintsoa-Haja <167571274+Rakotoarintsoa-Haja@users.noreply.github.com>
2024-09-07 11:21:41 +0300

    Update

---
+++ prototype_ip2.h
@@ -0,0 +1,8 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+unsigned int cidr_to_netmask(int cidr);
+void calcul_reseau(char* input);
+void print_ip(unsigned int ip)
+void afficher_formulaire();
